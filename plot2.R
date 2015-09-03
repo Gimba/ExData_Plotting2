63a3ee7027b9ffbace26d2704639601d24aec5ce
Martin Rosellen <martin.rosellen@fu-berlin.de>
2015-09-03 11:13:47 +0200

    plot2

---
+++ plot2.R
@@ -0,0 +1,12 @@
+plot2 <- function(){
+  #   NEI <- readRDS("summarySCC_PM25.rds")
+  #   SCC <- readRDS("Source_Classification_Code.rds")
+  sum1999  <- sum(NEI$Emissions, NEI$year == 1999 & NEI$fips == "24510")
+  sum2002  <- sum(NEI$Emissions, NEI$year == 2002 & NEI$fips == "24510")
+  sum2005  <- sum(NEI$Emissions, NEI$year == 2005 & NEI$fips == "24510")
+  sum2008  <- sum(NEI$Emissions, NEI$year == 2008 & NEI$fips == "24510")
+  emission <- c(sum1999, sum2002, sum2005, sum2008)
+  years  <- c(1999,2002,2005,2008)
+  plot(years,emission, type = "l", xaxt = "n")
+  axis(1, at = c(1999,2002, 2005, 2008))
+}
